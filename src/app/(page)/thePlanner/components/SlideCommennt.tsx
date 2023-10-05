d56f6742a43571670668456618ec774786e01b36
ZanDuc <118551805+NguyenZanDuc@users.noreply.github.com>
2023-10-05 20:07:46 +0700

    finish layout

--- src/app/(page)/thePlanner/components/SlideCommennt.tsx
+++ src/app/(page)/thePlanner/components/SlideCommennt.tsx
@@ -3,48 +3,52 @@ import Image from 'next/image';
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
-import { GrFormPrevious, GrFormNext } from 'react-icons/gr';
 
 const SlideCommnet = () => {
 
-   
-    
+
+
     const properties = {
-        prevArrow: <GrFormPrevious className="w-10 h-10  rounded-full bg-slate-200 bg-opacity-20" />,
-        nextArrow: <GrFormNext className="w-10 h-10  rounded-full bg-slate-200 bg-opacity-20"/>,
-        indicators: true,
+      
     }
     const comments = [{
-        avatar: "",
-        content: "Tasteful, distinctive and unforgettable wedding and events",
-        name: "The Planners",
+        avatar: "https://theplannersvn.com/wp-content/uploads/2023/09/R02_4140.jpg",
+        content: "Planning my wedding was so much less stressful, thanks to The Planners. For 7 months of the planning process, Van was my bridal stylist and sister. She always took care of me, my health and beauty, even when she was",
+        name: "- Eliza & Jason -",
+    }, {
+        avatar: "https://theplannersvn.com/wp-content/uploads/2023/09/365793457_697370315757143_999410349451155805_n.jpeg",
+        content: "No matter how many times I could choose my wedding planner, I would go with The Planners every time. Once again, I thank the best wedding planner ever, Ms. Van Nguyen, for always finding solutions to all of our difficult",
+        name: "- Trang & Steplen -",
+    }, {
+        avatar: "https://theplannersvn.com/wp-content/uploads/2023/08/TS104-1.jpg",
+        content: "I’ve followed and been a fan of The Planners closely for years. I love how all of your weddings are elegant, classy, detail-oriented and unique in their own way. I truly admire and I’m grateful for the energy of The",
+        name: "- Huong & Phong -",
+    }, {
+        avatar: "https://theplannersvn.com/wp-content/uploads/2023/09/R02_4140.jpg",
+        content: "We want to send you our biggest, happiest and beachiest THANK YOU for making our wedding day an absolute dream come true. Seriously, from the moment we met you, we knew we were in for something special. Your infectious enthusiasm...",
+        name: "- Thao & Bao -",
+    }, {
+        avatar: "https://theplannersvn.com/wp-content/uploads/2023/09/TIE_3962_websize-1.jpg",
+        content: "Quynh Anh and her team at The Planners provided me with tremendous support and consulting throughout the planning process and wedding day. Quynh Anh was truly the best companion I could ask for! It almost felt like some sort of...",
+        name: "- Quynh & Hung -",
+    }, {
+        avatar: "https://theplannersvn.com/wp-content/uploads/2023/09/361557580_683714420456066_8782850308229628398_n.jpeg",
+        content: "A BIG thank to The Planners Team for our beautiful, festive and emotional Wedding! We got what we wanted in our wedding: everybody had their good and memorial time! A super special appreciation to Thành & Quỳnh Anh for being...-",
+        name: "- Jack & Julia -",
     }]
     return (
         <div className='h-full w-screen text-white'>
-             <Slide {...properties} duration={4000} transitionDuration={2000} easing={"ease-out"}  >
-            <div className="flex flex-col justify-center text-black w-2/3 mx-auto items-center gap-3" >
-                <Image src={"https://theplannersvn.com/wp-content/uploads/2023/09/R02_4140.jpg"} className=' rounded-full' style={{objectFit:"cover", width:"90px", height:"90px"}} width={90} height={90} alt={''} />
-                <p className='py-4 text-center text-xl'>Planning my wedding was so much less stressful, thanks to The Planners. For 7 months of the planning process, Van was my bridal stylist and sister. She always took care of me, my health and beauty, even when she was</p>
-                <p className='text-xl text-center font-thin'>- Eliza & Jason -</p>
-            </div>
-            <div className="flex flex-col justify-center text-black w-2/3 mx-auto items-center gap-3" >
-                <Image src={"https://theplannersvn.com/wp-content/uploads/2023/09/R02_4140.jpg"} className=' rounded-full' style={{objectFit:"cover", width:"90px", height:"90px"}} width={90} height={90} alt={''} />
-                <p className='py-4 text-center text-xl'>Planning my wedding was so much less stressful, thanks to The Planners. For 7 months of the planning process, Van was my bridal stylist and sister. She always took care of me, my health and beauty, even when she was</p>
-                <p className='text-xl text-center font-thin'>- Eliza & Jason -</p>
-            </div>
-            <div className="flex flex-col justify-center text-black w-2/3 mx-auto items-center gap-3" >
-                <Image src={"https://theplannersvn.com/wp-content/uploads/2023/09/R02_4140.jpg"} className=' rounded-full' style={{objectFit:"cover", width:"90px", height:"90px"}} width={90} height={90} alt={''} />
-                <p className='py-4 text-center text-xl'>Planning my wedding was so much less stressful, thanks to The Planners. For 7 months of the planning process, Van was my bridal stylist and sister. She always took care of me, my health and beauty, even when she was</p>
-                <p className='text-xl text-center font-thin'>- Eliza & Jason -</p>
-            </div>
-            <div className="flex flex-col justify-center text-black w-2/3 mx-auto items-center gap-3" >
-                <Image src={"https://theplannersvn.com/wp-content/uploads/2023/09/R02_4140.jpg"} className=' rounded-full' style={{objectFit:"cover", width:"90px", height:"90px"}} width={90} height={90} alt={''} />
-                <p className='py-4 text-center text-xl'>Planning my wedding was so much less stressful, thanks to The Planners. For 7 months of the planning process, Van was my bridal stylist and sister. She always took care of me, my health and beauty, even when she was</p>
-                <p className='text-xl text-center font-thin'>- Eliza & Jason -</p>
-            </div>
-        </Slide>
+            <Slide  duration={4000} transitionDuration={2000} easing={"ease-out"}  >
+                {comments.map((comment, index) => (
+                        <div className="flex flex-col  justify-center text-black w-1/2 mx-auto items-center gap-3" key={index}>
+                            <Image src={comment.avatar} className=' rounded-full' style={{ objectFit: "cover", width: "90px", height: "90px" }} width={90} height={90} alt={''} />
+                            <p className='py-4 text-center text-xl'>{comment.content}</p>
+                            <p className='text-xl text-center font-thin'>{comment.name}</p>
+                        </div>
+                ))}
+            </Slide>
         </div>
-       
+
     );
 };
 
--- src/app/(page)/thePlanner/page.tsx
+++ src/app/(page)/thePlanner/page.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import SlideImage from './components/SlideImage'
-import { type } from 'os'
 import { cn } from '@/app/lib/utils'
 import SlideCommnet from './components/SlideCommennt'
+import { AiOutlineArrowRight } from 'react-icons/ai'
 
 type Props = {}
 
@@ -16,14 +16,14 @@ const page = (props: Props) => {
             <Service />
             <Content />
             <Comment />
-            {/* More */}
-            {/* Footer */}
+            <Contact />
+            <Footer />
         </div>
     )
 }
 
 export default page
-//https://theplannersvn.com/wp-content/uploads/2022/04/mainlogo-tp-01.svg
+
 const Navbar = () => {
     return (
         <div className='w-full flex justify-center gap-14 relative  items-center bg-thePlannerBackground py-4'>
@@ -49,10 +49,13 @@ const Hero = () => {
                     <p className='text-5xl w-[90%] text-center font-inconsolata font-light text-[#333333]'>Tasteful, distinctive and unforgettable wedding and events</p>
                 </div>
                 <div className='flex-1 space-y-8'>
-                    <p className='text-md'>Là một trong những wedding planner cao cấp và chuyên nghiệp nhất tại Việt Nam, The Planners cung cấp dịch vụ tư vấn, lập kế hoạch, thiết kế và tổ chức lễ cưới trọn gói tới các khách hàng trong nước và quốc tế tại nhiều địa điểm xuyên suốt Việt Nam. Với dịch vụ cao cấp thiết kế dựa trên yêu cầu cụ thể của từng khách hàng, mỗi đám cưới do The Planners tổ chức luôn đẹp, độc đáo và đáng nhớ không chỉ với các cặp đôi mà còn để lại ấn tượng sâu sắc với các khách mời.</p>
-                    <button className='rounded-sm text-white px-3 py-2 relative bg-thePlanner group w-[140px]  hover:w-[150px] duration-300 overflow-hidden flex justify-start items-center '>
+                    <p className='text-md'>Là một trong những wedding planner cao cấp và chuyên nghiệp nhất tại Việt Nam, The Planners cung cấp dịch vụ tư vấn, lập kế hoạch, thiết kế và tổ chức lễ cưới trọn gói tới các khách hàng trong nước và quốc tế tại nhiều địa điểm xuyên suốt Việt Nam.
+                        <span className='md:inline hidden'>Với dịch vụ cao cấp thiết kế dựa trên yêu cầu cụ thể của từng khách hàng, mỗi đám cưới do The Planners tổ chức luôn đẹp, độc đáo và đáng nhớ không chỉ với các cặp đôi mà còn để lại ấn tượng sâu sắc với các khách mời.
+                        </span>
+                    </p>
+                    <button className='rounded-sm text-white hover:scale-105 px-3 py-2 relative bg-thePlanner group w-[140px]  hover:w-[150px] duration-300 overflow-hidden flex justify-start items-center '>
                         <p className='flex-1'> TALK TO US</p>
-                        <span>ok</span>
+                        <AiOutlineArrowRight />
                         <div className='h-full z-10 w-[30px] absolute top-0 bg-thePlanner group-hover:w-0 duration-1000 right-0'></div>
                     </button>
                 </div>
@@ -63,14 +66,14 @@ const Hero = () => {
 
 const Service = () => {
     return (
-        <div className='md:h-[600px]  bg-thePlannerBackground w-full md:pt-[320px] pt-[230px]'>
+        <div className='md:h-[600px]  bg-thePlannerBackground w-full md:pt-[320px] pt-[250px]'>
             <div className='md:space-y-10 space-y-5'>
                 <p className='text-center text-xl'>| DỊCH VỤ |</p>
                 <p className='w-[70%] mx-auto text-center text-md'>Các dịch vụ của The Planners được thiết kế dựa trên yêu cầu cụ thể của từng khách hàng. Chúng tôi biết rằng mỗi đám cưới là riêng biệt và điều mà các khách hàng mong đợi từ The Planners là sự hỗ trợ chuyên nghiệp, chi tiết với yêu cầu cá nhân hoá cao.</p>
-                <div className='flex justify-center gap-20 divide-x-2 md:text-5xl text-xl'>
-                    <p className='w-[300px] text-center cursor-pointer'>Wedding Planning</p>
-                    <p className='w-[300px] text-center cursor-pointer'>Destination Wedding</p>
-                    <p className='w-[300px] text-center cursor-pointer'>Styling & Decoration</p>
+                <div className='flex justify-center md:gap-20 gap-5 pb-5 divide-x-2 md:text-5xl text-sm'>
+                    <p className='md:w-[300px] text-center cursor-pointer'>Wedding Planning</p>
+                    <p className='md:w-[300px] text-center cursor-pointer'>Destination Wedding</p>
+                    <p className='md:w-[300px] text-center cursor-pointer'>Styling & Decoration</p>
                 </div>
             </div>
         </div>
@@ -100,9 +103,9 @@ const Content = () => {
                         <p className='text-5xl'>Styling &</p>
                         <p className='px-32 text-5xl'>Decor Works</p>
                         <p className='text-sm font-mono w-[90%]'>Chúng tôi luôn tìm thấy rất nhiều niềm vui khi được sáng tạo và mang tới những điều xinh đẹp. Từ một buổi cầu hôn đến những bữa tiệc theo chủ đề hay styling cho một buổi chụp ảnh, chúng tôi luôn hào hứng khi được tận dụng trí tưởng tượng và tạo nên những khoảnh khắc đáng nhớ.</p>
-                        <button className='rounded-sm text-white px-3 py-2 relative bg-thePlanner group w-[190px]  hover:w-[210px] duration-300 overflow-hidden flex justify-start items-center '>
+                        <button className='rounded-sm text-white hover:scale-105 px-3 py-2 relative bg-thePlanner group w-[190px]  hover:w-[210px] duration-300 overflow-hidden flex justify-start items-center '>
                             <p className='flex-1'> EXPLORE DECOR</p>
-                            <span>ok</span>
+                            <AiOutlineArrowRight />
                             <div className='h-full z-10 w-[30px] absolute top-0 bg-thePlanner group-hover:w-0 duration-1000 right-0'></div>
                         </button>
                     </div>
@@ -153,13 +156,13 @@ const ContentItem = ({ image, name, title, position }: ContentItemProps) => {
     }
     return (
         <div className='relative'>
-            <Image src={image} alt={name} width={300} height={650} style={{ width: "100%", height: 650, borderRadius:10 }} />
+            <Image src={image} alt={name} width={300} height={650} style={{ width: "100%", height: 650, borderRadius: 10 }} />
             <div className={cn('rounded-sm w-[280px] bg-thePlannerBackground shadow-md p-4 space-y-2 absolute md:inline hidden ', getPosition(position))}>
                 <p className='text-sm font-inconsolata'>{name}</p>
                 <p className='text-lg font-serif'>{title}</p>
                 <button> View More</button>
             </div>
-            <div className={cn('rounded-sm w-[280px] bg-thePlannerBackground shadow-md p-4 space-y-2 absolute md:hidden bottom-2 right-2 opacity-80 ', )}>
+            <div className={cn('rounded-sm w-[280px] bg-thePlannerBackground shadow-md p-4 space-y-2 absolute md:hidden bottom-2 right-2 opacity-80 ',)}>
                 <p className='text-sm font-inconsolata'>{name}</p>
                 <p className='text-lg font-serif'>{title}</p>
                 <button> View More</button>
@@ -168,8 +171,8 @@ const ContentItem = ({ image, name, title, position }: ContentItemProps) => {
     )
 }
 const Comment = () => {
-    return(
-        <div className='w-screen space-y-10'>
+    return (
+        <div className='w-screen space-y-10 py-10 mb-10 shadow-sm bg-thePlannerBackground'>
             <p className='text-center'>| PRESS & PRAISE |</p>
             <SlideCommnet />
             <div className='w-full justify-center items-center flex gap-8'>
@@ -179,10 +182,56 @@ const Comment = () => {
         </div>
     )
 }
-//  Wedding
-// Planning
-// Destination
-// Wedding
-// Styling &
-// Decoration
-//VIEW PRESS
+const Contact = () => {
+    return (
+        <div className='w-screen relative'>
+            <Image src='https://theplannersvn.com/wp-content/uploads/2022/02/TU-NGUYEN-DESTINATION-WEDDING-PHU-QUOC-FRANKY-SEAN-07163905-2.jpg'
+                style={{ width: '100%', height: "570px", objectFit: 'cover' }}
+                className='shadow-sm'
+                alt='logo' width={1000} height={1000} />
+            <div className='md:w-[60%] w-[90%] absolute top-full -translate-y-1/3 left-1/2 -translate-x-1/2  md:py-14 md:px-28 py-4 px-4 bg-thePlannerBackground shadow-sm flex flex-col justify-center items-center gap-6'>
+                <p className='font-serif text-center text-5xl'>Looking</p>
+                <p className='md:text-3xl text-center'>for a professional team to take care ofyour wedding?</p>
+                <button className='rounded-sm text-white hover:scale-105 px-3 py-2 relative bg-thePlanner group w-[140px]  hover:w-[150px] duration-300 overflow-hidden flex justify-start items-center '>
+                    <p className='flex-1'> CONTACT US</p>
+                    <AiOutlineArrowRight />
+                    <div className='h-full z-10 w-[30px] absolute top-0 bg-thePlanner group-hover:w-0 duration-1000 right-0'></div>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div className='w-screen flex md:px-14 px-4  md:py-10 py-4 justify-between mt-64 text-sm bg-thePlannerBackground'>
+            <Link className='' href="/" >
+                <Image src="/mainlogo-tp-01.svg" alt="logo" width={200} height={200} />
+            </Link>
+            <div className='md:inline hidden font-light'>
+                <div>logo social</div>
+                <p>+84 123 456 789</p>
+                <p>develop@nikeys.com</p>
+                <p>Hanoi: 2nd floor, 1C Dang Thai Than, Hoan Kiem.</p>
+            </div>
+            <div className='md:inline hidden'>
+                <p className='font-semibold'>ABOUT</p>
+                <p className='font-semibold'>OUR WORKS</p>
+                <p>Wedding Works</p>
+                <p>Styling & Decoration</p>
+            </div>
+            <div className='md:inline hidden'>
+                <p className='font-semibold'>OUR SERVICES</p>
+                <p>Wedding Planning</p>
+                <p>Destination Wedding</p>
+                <p>Styling & Decoration</p>
+            </div>
+            <div>
+                <p className='font-semibold'>PRESS & PRAISE</p>
+                <p className='font-semibold'>BLOG</p>
+                <p className='font-semibold'>CONTACT</p>
+                <p className='font-semibold'>FAQ</p>
+            </div>
+        </div>
+    )
+}
